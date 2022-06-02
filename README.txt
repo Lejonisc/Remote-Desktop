2469e8b29818ff344d8b512155ed7be58a2fad26
Lejon <105602186+Lejonisc@users.noreply.github.com>
2022-06-02 19:22:36 +0300

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,3 @@
+Thanks for choosing this Remote Desktop.
+It isn't finished jet but you will get updated when is a new version.
+Lejon!
---
+++ sender.py
@@ -0,0 +1,14 @@
+from vidstream import ScreenShareClient
+import threading
+sender = ScreenShareClient('localhost', 9999)
+t = threading.Thread(target = sender.start_stream)
+t.start()
+
+
+
+
+
+while input("") != 'STOP':
+	continue
+sender.start_stream()
+receiver.stop_stream()
